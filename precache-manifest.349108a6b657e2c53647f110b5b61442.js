3111f80d0e1e087e69e8661140dc4a561353467d
Michael Martin <mictin3@gmail.com>
2020-08-20 10:56:13 -0600

    Updates

---
+++ precache-manifest.349108a6b657e2c53647f110b5b61442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6751ae11649bbc4a2ec83a1f418225a",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "99bcbbadbbbc284237bc",
+    "url": "/simple-todo-app/static/css/main.37903bf2.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99bcbbadbbbc284237bc",
+    "url": "/simple-todo-app/static/js/main.4e4077ab.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
